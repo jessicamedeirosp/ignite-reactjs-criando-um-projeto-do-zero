8400f634d949c5174b59a4e139783daba691a551
Jessica Medeiros <jessicamedeirosp96@yahoo.com.br>
2021-09-23 18:22:57 -0300

    feat: add component header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,3 +1,12 @@
+import styles from './header.module.scss';
+import commonStyles from '../../styles/common.module.scss';
+
 export default function Header() {
-  // TODO
+  return (
+    <header className={styles.header}>
+      <div className={commonStyles.container}>
+        <img src="/images/logo.svg" alt="spacetraveling" />
+      </div>
+    </header>
+  );
 }
